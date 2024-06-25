70eab0c61643cbfaf355de0b53c6efe4a77897ce
Zeukrob <josebchrist@hotmail.com>
2024-06-24 21:43:09 -0500

    working in the Hours option for the Counter.

--- App.js
+++ App.js
@@ -28,12 +28,12 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator> 
-      {/**
+      
       <Stack.Screen 
           name="Login" 
           component={Login} 
           options={{ title: 'Iglesia Valle de Bendición' }} />
-    */}
+    
         <Stack.Screen 
           name="Home" 
           component={Home} 
