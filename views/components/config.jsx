f0a202dc2e39ac23c7f968344d1e9b2bc0c23241
Zeukrob <josebchrist@hotmail.com>
2024-08-29 18:43:36 -0500

    paying the novatada

---
+++ views/components/config.jsx
@@ -0,0 +1,32 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+import { APIKEY } from '@env';
+import {
+  getReactNativePersistence,
+  initializeAuth,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: APIKEY,
+  authDomain: "arig-4cbef.firebaseapp.com",
+  projectId: "arig-4cbef",
+  storageBucket: "arig-4cbef.appspot.com",
+  messagingSenderId: "915549375802",
+  appId: "1:915549375802:web:b27b050aa4051b5ebbd403",
+  measurementId: "G-GYB68HVQSQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+//Initializa firestore
+
+export const db = getFirestore(app)
+
+/**
+export const auth = initializeAuth(db, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
+  */
